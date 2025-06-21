e82a3eeea9a71b7436a1054f9e7a63993a49ebc1
Cotaroy Cheung <twinflame265@gmail.com>
2025-06-21 12:53:00 -0400

    streaks

--- saveload.py
+++ saveload.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime
+import datetime
 import json
 from user import User, Task
 
@@ -12,9 +12,18 @@ def save(user: User, file_path: str = FILEPATH) -> None:
         json.dump(user_data, f, ensure_ascii=False, indent=4)
 
 def load_user(file_path = FILEPATH) -> User:
+    """
+    >>> a = Task('say hello to the neighbours', datetime.datetime.now().date())
+    >>> tommy = User(tasks=[a])
+    >>> save(tommy, 'data/test.json')
+    >>> load_user('data/test.json')
+    User(streaks=0, finished_task_today=False, tasks=[Task(description='say hello to the neighbours', deadline=datetime.datetime(2025, 6, 21, 0, 0))], finished_tasks=[], finished_tutorial=False, recently_deleted=[])
+
+    """
     with open(file_path, 'r') as file:
         data = json.load(file)
-    return User(data['streaks'], load_tasks(data['tasks']), load_tasks(data['finished_tasks']), data['finished_tutorial'])
+    return User(data['streaks'], data['finished_task_today'], load_tasks(data['tasks']),
+                load_tasks(data['finished_tasks']), data['finished_tutorial'])
 
 def load_tasks(data: list):
-    return [Task(task['description'], datetime.strptime(task['deadline'], "%Y-%m-%d")) for task in data]
+    return [Task(task['description'], datetime.datetime.strptime(task['deadline'], "%Y-%m-%d")) for task in data]
